30e8cd3a08318d67a730529dcfc374ec13943a6c
Desmond Parker <evan.parker95@gmail.com>
2021-12-01 18:45:55 +0000

    adding client.js and refactoring play.js for snake-client project

--- play.js
+++ play.js
@@ -1,21 +1,5 @@
 const net = require("net");
-
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 50541,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on("connect", () => {
-    console.log("example")
-  });
-  conn.on('data', (data) => {
-    console.log('Server says: ', data);
-  });
-  return conn;
-};
+const connect = require('./client')
 
 console.log("Connecting ...");
 connect();
